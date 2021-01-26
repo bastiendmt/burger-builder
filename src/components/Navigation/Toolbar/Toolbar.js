42225e16dd5bdb9091c5b8843db99b234abce757
bastienxs <bastienxs@gmail.com>
2021-01-26 18:03:41 +0100

    icon click home page

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
-const toolbar = ( props ) => (
+const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <DrawerToggle clicked={props.drawerToggleClicked} />
-        <div className={classes.Logo}>
+        <NavLink
+            className={classes.Logo}
+            to='/'>
             <Logo />
-        </div>
+        </NavLink>
         <nav className={classes.DesktopOnly}>
             <NavigationItems isAuth={props.isAuth} />
         </nav>
