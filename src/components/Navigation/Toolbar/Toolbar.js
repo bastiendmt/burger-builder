126b75409faff7cd357393e82a699bfd9d6601dc
bastienxs <bastienxs@gmail.com>
2021-04-22 22:51:46 +0200

    indent

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-import classes from './Toolbar.module.css';
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems';
-import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+import classes from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 const toolbar = (props) => (
-    <header className={classes.Toolbar}>
-        <DrawerToggle clicked={props.drawerToggleClicked} />
-        <div className={classes.ToolbarContainer}>
-        <NavLink
-            className={classes.Logo}
-            to='/'>
-            <Logo />
-        </NavLink>
-        <nav className={classes.DesktopOnly}>
-            <NavigationItems isAuth={props.isAuth} />
-        </nav>
-                </div>
-    </header>
+  <header className={classes.Toolbar}>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
+    <div className={classes.ToolbarContainer}>
+      <NavLink className={classes.Logo} to="/">
+        <Logo />
+      </NavLink>
+      <nav className={classes.DesktopOnly}>
+        <NavigationItems isAuth={props.isAuth} />
+      </nav>
+    </div>
+  </header>
 );
 
 export default toolbar;
